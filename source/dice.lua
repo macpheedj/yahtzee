58f6431cead6de34af69ce10b7b6bf634d6b349d
Daniel MacPhee <macpheed3@gmail.com>
2024-08-30 00:50:52 -0400

    dice rolling

--- source/dice.lua
+++ source/dice.lua
@@ -1,3 +1,4 @@
+local pd <const> = playdate
 local gfx <const> = playdate.graphics
 
 class("Dice").extends(gfx.sprite)
@@ -6,15 +7,54 @@ function Dice:init()
 	Dice.super.init(self)
 
 	self.isHeld = false
+	self.isRolling = false
+	self.value = 1
 	self.images = gfx.imagetable.new("images/die")
 
-	self:randomizeDie()
+	self:setupRoller()
+	self:randomizeValue()
 end
 
-function Dice:randomizeDie()
-	local rng = math.random(6)
-	local image = self.images:getImage(rng)
-	self:setImage(image)
+function Dice:randomizeValue()
+	local previousValue = self.value
+
+	-- no repeats
+	while self.value == previousValue do
+		self.value = math.random(6)
+	end
+
+	self:setImage(self.images:getImage(self.value))
+end
+
+function Dice:setupRoller()
+	self.roller = playdate.timer.new(50)
+	self.roller.repeats = true
+	self.roller.timerEndedCallback = function()
+		self:randomizeValue()
+	end
+
+	self.roller:pause()
+end
+
+function Dice:startRolling()
+	if self.isRolling or self.isHeld then return end
+
+	self.isRolling = true
+	self.roller:start()
+end
+
+function Dice:stopRolling()
+	if not self.isRolling then return end
+
+	self.isRolling = false
+	self.roller:pause()
+end
+
+function Dice:update()
+	if DEBUG then
+		if pd.buttonJustPressed(pd.kButtonA) then self:startRolling() end
+		if pd.buttonJustPressed(pd.kButtonB) then self:stopRolling() end
+	end
 end
 
 return Dice
--- source/main.lua
+++ source/main.lua
@@ -5,15 +5,34 @@ import "CoreLibs/timer"
 
 import "dice"
 
+DEBUG = true
+
 local gfx <const> = playdate.graphics
+
+function setupDice()
+	local dice = {}
+	local scale = 1.5
+	local width = 48 * scale
+	local margin = 4
+	local padding = margin * 2
+	local start_position = ((48 / scale) * scale) - margin
+
+	for i = 1, 5 do
+		dice[i] = Dice()
+		local offset = ((i - 1) * width) + (i * margin)
+		dice[i]:setScale(scale)
+		dice[i]:moveTo(start_position + offset, 240 - width / 2 - padding)
+		dice[i]:add()
+	end
+
+	return dice
+end
 
 function gameStart()
 	print("hello world")
 	math.randomseed(playdate.getSecondsSinceEpoch())
 
-	local die = Dice()
-	die:moveTo(200, 120)
-	die:add()
+	setupDice()
 end
 
 gameStart()
